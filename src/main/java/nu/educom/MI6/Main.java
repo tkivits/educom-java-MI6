87af25283bd429e9d3e48c2954225656c89637d5
tkivits <t.kivits@hotmail.com>
2022-08-17 15:40:46 +0200

    Issue#1: Console applicatie compleet werkend.

--- src/main/java/nu/educom/MI6/Main.java
+++ src/main/java/nu/educom/MI6/Main.java
@@ -12,22 +12,28 @@ public class Main {
     Scanner input = new Scanner(System.in);
 
     while(loop) {
-
       System.out.println("Please input your service number:");
       String serviceNumber = input.nextLine();
 
-      if(isLoggedOn.contains(serviceNumber)) {
-        System.out.println("You are already logged on.");
-      } else {
-        checkServiceNumber(serviceNumber);
+      if(serviceNumber.length() < 3) {
+        serviceNumber = leftPadWithZeros(serviceNumber);
       }
 
-    }
-
-    System.out.println("Please enter our super secret password:");
-
-    String password = input.nextLine();
-    checkPassword(password);
+      if(isBlacklisted.contains(serviceNumber)) {
+        System.out.println("ERROR: You are blacklisted!");
+        System.out.println(" ");
+      } else if(isLoggedOn.contains(serviceNumber)) {
+          System.out.println("You are already logged on.");
+          System.out.println(" ");
+        } else {
+          serviceNumber = checkServiceNumber(serviceNumber);
+          if(serviceNumber != null) {
+            System.out.println("Please enter our super secret password:");
+            String password = input.nextLine();
+            checkPassword(password, serviceNumber);
+          }
+        }
+      }
     }
   protected static boolean isNumeric(String num) {
     if(num == null) {
@@ -43,10 +49,7 @@ public class Main {
   protected static String leftPadWithZeros(String str) {
     return String.format("%0"+ (3 - str.length() )+"d%s",0 ,str);
   }
-  public static void checkServiceNumber(String serviceNumber) {
-    if(serviceNumber.length() < 3) {
-      serviceNumber = leftPadWithZeros(serviceNumber);
-    }
+  protected static String checkServiceNumber(String serviceNumber) {
     try{
       if (!isNumeric(serviceNumber)) {
         throw new Exception("Input is incorrect.");
@@ -56,15 +59,21 @@ public class Main {
       }
     } catch (Exception e) {
       System.out.println("Input is incorrect.");
+      System.out.println(" ");
+      return null;
     }
-    isLoggedOn.add(serviceNumber);
+    return serviceNumber;
   }
-  protected static void checkPassword(String password) {
+  protected static void checkPassword(String password, String serviceNumber) {
     String actualPassword = "For ThE Royal QUEEN";
     if(actualPassword.equals(password)) {
-      System.out.println("You entered the correct password");
+      isLoggedOn.add(serviceNumber);
+      System.out.println("You are now logged on, agent " + serviceNumber);
+      System.out.println(" ");
     } else {
+      isBlacklisted.add(serviceNumber);
       System.out.println("You are now blacklisted!");
+      System.out.println(" ");
     }
   }
 }
