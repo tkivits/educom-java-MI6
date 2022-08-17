77fcdd599e15b5395c539db83d5689daba520b3a
tkivits <t.kivits@hotmail.com>
2022-08-17 11:49:13 +0200

    Issue#1: Nummer kan ingevoerd worden.

--- src/main/java/nu/educom/MI6/Main.java
+++ src/main/java/nu/educom/MI6/Main.java
@@ -1,7 +1,21 @@
 package nu.educom.MI6;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please input your service number:");
+
+    try {
+      int serviceNumber = input.nextInt();
+      if(String.valueOf(serviceNumber).length() > 3) {
+        System.out.println("Input is incorrect.");
+      }
+      if(String.valueOf(serviceNumber).length() < 3) {
+        String paddedSerNum = String.format("%03d", serviceNumber);
+      }
+    } catch (Exception e) {
+      System.out.println("Input is incorrect.");
+    }
   }
 }
