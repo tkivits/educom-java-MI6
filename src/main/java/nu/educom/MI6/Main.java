57d7cf47bc1eec47e2a1fd9ebde44ab7c47cd4e4
tkivits <t.kivits@hotmail.com>
2022-08-17 13:00:27 +0200

    Issue#1: Nummer invoer verbeterd.

--- src/main/java/nu/educom/MI6/Main.java
+++ src/main/java/nu/educom/MI6/Main.java
@@ -6,16 +6,42 @@ public class Main {
     Scanner input = new Scanner(System.in);
     System.out.println("Please input your service number:");
 
+    String serviceNumber = input.nextLine();
+    String result = checkServiceNumber(serviceNumber);
+
+    if(result != null) {
+      System.out.println("Welcome agent " + result);
+      System.out.println("Please enter our super secret password:");
+    }
+    }
+  public static boolean isNumeric(String num) {
+    if(num == null) {
+      return false;
+    }
     try {
-      int serviceNumber = input.nextInt();
-      if(String.valueOf(serviceNumber).length() > 3) {
-        System.out.println("Input is incorrect.");
+      int i = Integer.parseInt(num);
+    } catch (NumberFormatException e) {
+      return false;
+    }
+    return true;
+  }
+  public static String leftPadWithZeros(String str) {
+    return String.format("%0"+ (3 - str.length() )+"d%s",0 ,str);
+  }
+  public static String checkServiceNumber(String serviceNumber) {
+    if(serviceNumber.length() < 3) {
+      serviceNumber = leftPadWithZeros(serviceNumber);
+    }
+    try{
+      if (!isNumeric(serviceNumber)) {
+        throw new Exception("Input is incorrect.");
       }
-      if(String.valueOf(serviceNumber).length() < 3) {
-        String paddedSerNum = String.format("%03d", serviceNumber);
+      if (serviceNumber.length() > 3) {
+        throw new Exception("Input is incorrect.");
       }
     } catch (Exception e) {
       System.out.println("Input is incorrect.");
     }
+    return serviceNumber;
   }
 }
