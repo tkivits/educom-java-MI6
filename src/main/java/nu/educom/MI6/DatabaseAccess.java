255aebaf6e5953ce22e44839084bdf5f828a0d42
tkivits <t.kivits@hotmail.com>
2022-08-22 16:03:16 +0200

    Issue#8: bestanden verplaatst.

--- src/main/java/nu/educom/MI6/DatabaseAccess.java
+++ src/main/java/nu/educom/MI6/DatabaseAccess.java
@@ -1,6 +1,32 @@
 package nu.educom.MI6;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseAccess {
-    private String dbUser = "MI6User";
-    private String dbPass = "vmCyOY(0LnYN_K3-";
+
+    public Connection initiateConnection() {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url       = "jdbc:mysql://localhost:4400/mi6";
+            String user      = "MI6User";
+            String password  = "vmCyOY(0LnYN_K3-";
+
+            // create a connection to the database
+            conn = DriverManager.getConnection(url, user, password);
+            // more processing here
+            // ...
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try{
+                if(conn == null);
+                conn.close();
+            }catch(SQLException ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
 }
