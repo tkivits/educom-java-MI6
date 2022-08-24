a08c7598b37312685f175aa7a4e96f0bd631a24c
tkivits <t.kivits@hotmail.com>
2022-08-24 10:17:00 +0200

    Issue#5: pogingen login worden opgehaald uit database.

--- src/main/java/nu/educom/MI6/LoginAttempt.java
+++ src/main/java/nu/educom/MI6/LoginAttempt.java
@@ -1,12 +1,23 @@
 package nu.educom.MI6;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginAttempt {
+
+    private String serviceNumber;
+    private Timestamp dateTime;
+    private boolean success;
+    private int id;
+
+    public LoginAttempt(int id, String serviceNumber, Timestamp dateTime, boolean success) {
+        this.id = id;
+        this.serviceNumber = serviceNumber;
+        this.dateTime = dateTime;
+        this.success = success;
+    }
+
     public static void insertLoginAttempt(String serviceNumber, int success) {
         long now = System.currentTimeMillis();
         Timestamp dateTime = new Timestamp(now);
@@ -21,5 +32,24 @@ public class LoginAttempt {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public static List<LoginAttempt> getLoginAttempts(int serviceNumber) {
+        List<LoginAttempt> loginAttempts = new ArrayList<>();
+
+        try {
+            Connection conn = nu.educom.MI6.Connector.createConn();
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM login_attempts WHERE service_num = ? AND ID > IFNULL((SELECT MAX(id) FROM login_attempts WHERE success = 1 AND service_num = ?), 0)");
+            stmt.setInt(1, serviceNumber);
+            stmt.setInt(2, serviceNumber);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                loginAttempts.add(new LoginAttempt(rs.getInt("ID"), rs.getString("service_num"), rs.getTimestamp("time_login"), rs.getBoolean("success")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return loginAttempts;
     }
 }
